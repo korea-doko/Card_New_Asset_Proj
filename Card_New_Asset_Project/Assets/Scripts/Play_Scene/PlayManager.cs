03bf529ca83e6d3552bfdb770871fcedade557e4
Unknown <w9716ww@naver.com>
2019-10-28 22:21:30 +0900

    PlayScene에서 배경 Sprite 화면 비율에 상관없이 항상 꽉 찰 수 있게
    PlayScene에서 화면 배경의 사이즈가 항상 스크린 비율에 상관없이 꽉 찰 수 있게 만듬

--- Card_New_Asset_Project/Assets/Scripts/Play_Scene/PlayManager.cs
+++ Card_New_Asset_Project/Assets/Scripts/Play_Scene/PlayManager.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class PlayManager : MonoBehaviour
 {
+    public SpriteRenderer bgSpr;
+
+    public float defaultWidth;
+    public float defaultHeight;
+
     // Start is called before the first frame update
     void Start()
     {
         
+        float worldHeight = Camera.main.orthographicSize * 2.0f;
+        float worldWidth = worldHeight / Screen.height * Screen.width;
+
+        bgSpr.transform.localScale =
+            new Vector3(
+                worldWidth / bgSpr.sprite.bounds.size.x,
+                worldHeight / bgSpr.sprite.bounds.size.y,
+                1
+                );
+
     }
 
     // Update is called once per frame
